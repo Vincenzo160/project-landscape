84753b6d65ef39079a6520dd084c69f856ff9f52
Vincenzo Deluca <delucaavincenzo@gmail.com>
2021-08-02 21:23:53 +0200

    Update to 1.1

--- Landscape/landscapeMacOS.py
+++ Landscape/landscapeMacOS.py
@@ -1,15 +1,19 @@
 # Landscape project by TheShadowG
 from os import get_exec_path
 import re
+from sys import version
 
-print ("Version: 1.1")
+version = '1.2'
+print ("Version: " + version)
 print ("Project landscape")
 print ("/!\ ATTENTION /!\ ")
 print ("If your client don't save the log in the minecraft folder this program won't function")
 pattern = 'You earned'
 matching_lines = [line for line in open('/Users/vincenzodeluca/Library/Application Support/minecraft/logs/latest.log').readlines() if pattern in line] # please edit /Users/vincenzodeluca/Library/...
  #and replace vincenzodeluca whit your user (You can find what is your user by pressing Shift+Cmd+G on the finder, and typing /users and seeing what is the name of your user)
-
+if matching_lines == []:
+    print ("")
+    print ("No GEXP Found, if you think this is an error please report it on https://bit.ly/reportnoGEXP")
 print ("Select from this menu")
 print ("Show [G]exp earned:")
 print ("Show [T]otal Gexp earned:")
