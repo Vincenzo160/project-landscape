1bd52e8a4b48d50c6cccca52868c1741154da31b
Vincenzo Deluca <delucaavincenzo@gmail.com>
2021-08-08 13:05:27 +0200

    Update 1.3

--- Landscape/landscapeMacOS.py
+++ Landscape/landscapeMacOS.py
@@ -1,48 +1,57 @@
-# Landscape project by TheShadowG
-from os import get_exec_path
+try:
+    import Tkinter as tk
+except:
+    import tkinter as tk
+from os import times
 import re
-from sys import version
 
-version = '1.2'
+
+
+version = '1.3 [BETA]'
+
+path = '/Users/vincenzodeluca/Library/Application Support/minecraft/logs/latest.log'
+
 print ("Version: " + version)
 print ("Project landscape")
 print ("/!\ ATTENTION /!\ ")
 print ("If your client don't save the log in the minecraft folder this program won't function")
+
+root = tk.Tk()
+root.geometry("500x500")
+root.title('Project Landscape')
+root.resizable(False, False)
+root.iconbitmap()
+
+def close_window():
+    root.destroy()
+
+
 pattern = 'You earned'
-matching_lines = [line for line in open('/Users/vincenzodeluca/Library/Application Support/minecraft/logs/latest.log').readlines() if pattern in line] # please edit /Users/vincenzodeluca/Library/...
- #and replace vincenzodeluca whit your user (You can find what is your user by pressing Shift+Cmd+G on the finder, and typing /users and seeing what is the name of your user)
-if matching_lines == []:
-    print ("")
-    print ("No GEXP Found, if you think this is an error please report it on https://bit.ly/reportnoGEXP")
-    print ("")
-print ("Select from this menu")
-print ("Show [G]exp earned:")
-print ("Show [T]otal Gexp earned:")
-input = input(":")
-
-def list():
-    print("")
-    print("GEXP Earned:")
-    sum = 0
-    for s in matching_lines:
-        m = re.search("earned (\\d+) GEXP", s)
-        sum = sum + int(m.group(1))
-        print(m.group(1))
-    print("Total GEXP Earned = {}".format(sum))
-
-def ssum():
-    print("")
-    print("Total GEXP Earned:")
-    sum = 0
-    for s in matching_lines:
-        m = re.search("earned (\\d+) GEXP", s)
-        sum = sum + int(m.group(1))
-    print("GEXP Earned = {}".format(sum))
+matching_lines = [line for line in open(path).readlines() if pattern in line] # please edit /Users/vincenzodeluca/Library/...
+#and replace vincenzodeluca whit your user (You can find what is your user by pressing Shift+Cmd+G on the finder, and typing /users and seeing what is the name of your user)
+sum = 0
+for s in matching_lines:
+    m = re.search("earned (\\d+) GEXP", s)
+    sum = sum + int(m.group(1))
+    print(m.group(1))
+total = ("Total GEXP Earned = {}".format(sum))
+total = tk.Label(text="Total GEXP Earned = {}".format(sum), width=50, height=5)
+list = tk.Label(text="Last amount of GEXP: " + m.group(1), width=50, height=5)
 
 
-if input == "g" or input == "G" or input == "": list()
-if input == "t" or input == "T": ssum()
+version = tk.Label(text="Landscape Version: " + version)
+changept = tk.Button(text = "Quit", command = close_window)
+
+version.pack()
+list.pack()
+total.pack()
+
+#fetchr = tk.Button(text = "Refresh", command = fetch)
+#fetchr.pack()
+
+button = tk.Button(text = "Quit", command = exit)
+button.pack(side=tk.BOTTOM)
 
 
+root.mainloop()
 
-exit()
