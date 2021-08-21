3e6bc9eb1cac501a742e9c9a06465880b4eee93e
Vincenzo Deluca <delucaavincenzo@gmail.com>
2021-08-21 16:08:18 +0200

    Update 1.3 [BETA 3]

--- Landscape/Beta/landscapeWin.py
+++ Landscape/Beta/landscapeWin.py
@@ -4,10 +4,11 @@ except:
     import tkinter as tk
 from os import times
 import re
+import logging
+from datetime import datetime
 
 
-
-version = '1.3 [BETA 2]'
+version = '1.3 [BETA 3]'
 
 path = 'C:/Users/USERNAME/.minecraft/logs/latest.log' # please edit C:/Users/USERNAME/.minecraft/...
 #and replace USERNAME whit your user, info: https://github.com/Vincenzo160/project-landscape/wiki/Setting-the-path
@@ -26,6 +27,21 @@ root.iconbitmap()
 def close_window():
     root.destroy()
 
+def slog():
+    logging.basicConfig(filename="GEXP.log", level=logging.INFO)
+    print("")
+    print("Saving..")
+    now = datetime.now()
+    current_time = now.strftime("%Y %M %D %H:%M:%S")
+    print(current_time)
+    logging.info(current_time)
+    sum = 0
+    for s in matching_lines:
+        m = re.search("earned (\\d+) GEXP", s)
+        sum = sum + int(m.group(1))
+        logging.info(m.group(1))
+    logging.info("Total GEXP Earned = {}".format(sum))
+    print("GEXP.log Saved Succesfully in your user root")    
 
 pattern = 'You earned'
 matching_lines = [line for line in open(path).readlines() if pattern in line] 
@@ -41,17 +57,19 @@ list = tk.Label(text="Last amount of GEXP: " + m.group(1), width=50, height=5)
 
 version = tk.Label(text="Landscape Version: " + version)
 changept = tk.Button(text = "Quit", command = close_window)
+saveto = tk.Button(text = "Save To File", command = slog)
 
 version.pack()
 list.pack()
 total.pack()
+
 
 #fetchr = tk.Button(text = "Refresh", command = fetch)
 #fetchr.pack()
 
 button = tk.Button(text = "Quit", command = exit)
 button.pack(side=tk.BOTTOM)
-
+saveto.pack(side=tk.BOTTOM)
 
 root.mainloop()
 
